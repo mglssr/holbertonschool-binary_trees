54ef896c57e59348e3ec9064a82e7c24c2ae18a2
mglssr <iglesiasmaia6@gmail.com>
2022-07-17 12:43:55 -0700

    task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -17,6 +17,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return (NULL);
 	(*node).parent = parent;
 	(*node).n = value;
+	(*node).left = NULL;
+	(*node).right = NULL;
 
 	return (node);
 }
