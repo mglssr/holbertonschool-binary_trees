12d6dd4f3824ac7bccb2902dffa2209ce7c13c0b
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-19 07:28:46 -0700

    Fixing tasks 1 and 2

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -14,6 +14,8 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (!parent)
+		return (NULL);
 	new_node = binary_tree_node(parent, value);
 
 	if (!new_node)
