c0505b13ced92d2399467bb60738a8204280ce70
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-07-20 07:14:29 -0700

    Checking task 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -57,6 +57,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	height_left = binary_tree_height(tree->left);
 	height_right = binary_tree_height(tree->right);
 	if (height_left == height_right)
-		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+		return (binary_tree_is_perfect(tree->left) &&
+		binary_tree_is_perfect(tree->right));
 	return (0);
 }
